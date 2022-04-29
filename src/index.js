23395e3c584f13f591ed9c076ecac062942a3d29
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-04-29 15:36:32 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const connect = require("./config/db.js")
+const express = require("express");
+const cors =require("cors");
+
+const usercontroller = require("./controller/user.controller");
+const app = express();
+app.use(cors());
+app.use(express.static('fontend'));
+app.use(express.json());
+
+app.use("/users",usercontroller);
+
+
+app.listen(process.env.PORT, async()=>{
+     
+    try{
+        await connect();
+        console.log("Listening on port 5000");
+    }
+    catch(err){
+        console.log(err.message);
+    }
+    
+});
